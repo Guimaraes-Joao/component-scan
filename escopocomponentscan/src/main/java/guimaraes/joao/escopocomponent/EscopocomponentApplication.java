65b9e6ab148b215c74539ddc5fe1fe079ab57708
Guimaraes-Joao <63751467+Guimaraes-Joao@users.noreply.github.com>
2020-04-19 21:27:44 -0300

    Add files via upload

---
+++ escopocomponentscan/src/main/java/guimaraes/joao/escopocomponent/EscopocomponentApplication.java
@@ -0,0 +1,13 @@
+package guimaraes.joao.escopocomponent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EscopocomponentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EscopocomponentApplication.class, args);
+	}
+
+}
---
+++ escopocomponentscan/src/test/java/guimaraes/joao/escopocomponent/EscopocomponentApplicationTests.java
@@ -0,0 +1,13 @@
+package guimaraes.joao.escopocomponent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EscopocomponentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
